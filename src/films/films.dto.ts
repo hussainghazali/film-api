c3002ce88dffd51c0b74270ca155e429add72367
hussain <hussain@smash.cloud>
2023-05-13 20:09:49 +0500

    Updating the DTO with swagger Docs

--- src/films/films.dto.ts
+++ src/films/films.dto.ts
@@ -5,56 +5,71 @@ import {
   IsUrl,
   IsNumber,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateFilmDto {
+  @ApiProperty({ description: 'The name of the film.' })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({ description: 'The description of the film.' })
   @IsNotEmpty()
   @IsString()
   description: string;
 
+  @ApiProperty({ description: 'The release date of the film.' })
   @IsNotEmpty()
   @IsDateString()
   releaseDate: Date;
 
+  @ApiProperty({ description: 'The ticket price of the film.' })
   @IsNotEmpty()
   @IsNumber()
   ticketPrice: number;
 
+  @ApiProperty({ description: 'The country of the film.' })
   @IsNotEmpty()
   @IsString()
   country: string;
 
+  @ApiProperty({ description: 'The genre of the film.' })
   @IsNotEmpty()
   @IsString()
   genre: string;
 
+  @ApiProperty({ description: 'The URL of the film photo.' })
   @IsNotEmpty()
   @IsUrl()
   photo: string;
 }
 
 export class UpdateFilmDto {
+  @ApiProperty({ description: 'The name of the film.' })
   @IsString()
   name?: string;
 
+  @ApiProperty({ description: 'The description of the film.' })
   @IsString()
   description?: string;
 
+  @ApiProperty({ description: 'The release date of the film.' })
   @IsDateString()
   releaseDate?: Date;
 
+  @ApiProperty({ description: 'The ticket price of the film.' })
   @IsNumber()
   ticketPrice?: number;
 
+  @ApiProperty({ description: 'The country of the film.' })
   @IsString()
   country?: string;
 
+  @ApiProperty({ description: 'The genre of the film.' })
   @IsString()
   genre?: string;
 
+  @ApiProperty({ description: 'The URL of the film photo.' })
   @IsUrl()
   photo?: string;
 }

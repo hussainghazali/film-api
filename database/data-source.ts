4ccef2f871ce5fe20a68875dd92a27927d956cce
hussain <hussain@smash.cloud>
2023-05-14 04:51:37 +0500

    Enable the Logs

--- database/data-source.ts
+++ database/data-source.ts
@@ -1,6 +1,7 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { CreateFilmTable1683919139346 } from './migrations/1683919139346-CreateFilmTable';
 import { SeedFilmTable1683919139346 } from './migrations/1683978810184-SeedFilmTable1683919139346';
+import { Film } from 'src/films/films.entity';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
@@ -9,9 +10,10 @@ export const dataSourceOptions: DataSourceOptions = {
   username: 'root',
   password: '0513',
   database: 'dream_drivers',
-  entities: ['dist/**/*.entity.{js,ts}'],
+  entities: [Film],
   migrations: [CreateFilmTable1683919139346, SeedFilmTable1683919139346],
   synchronize: true,
+  logging: true,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
